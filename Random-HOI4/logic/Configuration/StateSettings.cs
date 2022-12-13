8ffeaec31102ce5a4d41b80a4cdd001f1b04448a
millennium <1543255825@qq.com>
2022-12-13 19:43:18 +0800

    改进JSON文件格式并修改相应的代码

--- Random-HOI4/logic/Configuration/StateSettings.cs
+++ Random-HOI4/logic/Configuration/StateSettings.cs
@@ -1,18 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Random_HOI4.logic.Configuration
 {
     public class StateSettings
     {
-        /// <summary>
-        /// Key为地块类别, Value为地块提供的建筑槽位
-        /// </summary>
-        public readonly Dictionary<string, byte>? StateCategory;
+        public List<StateCategory>? StateCategory { get; set; }
+        public List<Buildings>? Buildings { get; set; }
         public int MinManpower { get; set; }
         public int MaxManpower { get; set; }
+    }
+
+    public class StateCategory
+    {
+        public string? Type { get; set; }
+        public byte? Slot { get; set; }
+    }
+
+    public class Buildings
+    {
+        public string? BuildingName { get; set; }
+        public byte MaxLevel { get; set; }
+        public bool IsProvincesBuilding { get; set; }
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -14,21 +14,160 @@ namespace Test
             };
 
             /*
-             *  数据来自 https://hoi4.parawikis.com/wiki/%E5%9C%B0%E5%8C%BA%E6%A8%A1%E6%94%B9
+             * 地块类型.
+             * 数据来自 https://hoi4.parawikis.com/wiki/%E5%9C%B0%E5%8C%BA%E6%A8%A1%E6%94%B9
              */
-            state.StateCategory.Add("wasteland", 0);
-            state.StateCategory.Add("enclave", 0);
-            state.StateCategory.Add("tiny_island", 0);
-            state.StateCategory.Add("small_island", 1);
-            state.StateCategory.Add("pastoral", 1);
-            state.StateCategory.Add("rural", 2);
-            state.StateCategory.Add("town", 4);
-            state.StateCategory.Add("large_town", 5);
-            state.StateCategory.Add("city", 6);
-            state.StateCategory.Add("large_city", 8);
-            state.StateCategory.Add("metropolis", 10);
-            state.StateCategory.Add("megalopolis", 12);
-            
+            state.StateCategory.Add(new StateCategory() 
+            {
+                Type = "wasteland",
+                Slot = 0
+            });
+            state.StateCategory.Add(new StateCategory()
+            {
+                Type = "enclave",
+                Slot = 0
+            });
+            state.StateCategory.Add(new StateCategory()
+            {
+                Type = "tiny_island",
+                Slot = 0
+            });
+            state.StateCategory.Add(new StateCategory()
+            {
+                Type = "small_island",
+                Slot = 1
+            });
+            state.StateCategory.Add(new StateCategory()
+            {
+                Type = "pastoral",
+                Slot = 1
+            });
+            state.StateCategory.Add(new StateCategory()
+            {
+                Type = "rural",
+                Slot = 2
+            });
+            state.StateCategory.Add(new StateCategory()
+            {
+                Type = "town",
+                Slot = 4
+            });
+            state.StateCategory.Add(new StateCategory()
+            {
+                Type = "large_town",
+                Slot = 5
+            });
+            state.StateCategory.Add(new StateCategory()
+            {
+                Type = "city",
+                Slot = 6
+            });
+            state.StateCategory.Add(new StateCategory()
+            {
+                Type = "large_city",
+                Slot = 8
+            });
+            state.StateCategory.Add(new StateCategory()
+            {
+                Type = "metropolis",
+                Slot = 10
+            });
+            state.StateCategory.Add(new StateCategory()
+            {
+                Type = "megalopolis",
+                Slot = 12
+            });
+
+            /*
+             * 建筑类型.
+             * 数据来自HOI4文件, Path: Hearts of Iron IV\common\buildings\00_buildings.txt
+             * HOI4版本: 1.12.*
+             */
+            state.Buildings.Add(new Buildings() 
+            {
+                BuildingName = "infrastructure",
+                MaxLevel = 10,
+                IsProvincesBuilding = false
+            });
+            state.Buildings.Add(new Buildings()
+            {
+                BuildingName = "arms_factory",
+                MaxLevel = 20,
+                IsProvincesBuilding = false
+            });
+            state.Buildings.Add(new Buildings()
+            {
+                BuildingName = "industrial_complex",
+                MaxLevel = 20,
+                IsProvincesBuilding = false
+            });
+            state.Buildings.Add(new Buildings()
+            {
+                BuildingName = "air_base",
+                MaxLevel = 10,
+                IsProvincesBuilding = false
+            });
+            state.Buildings.Add(new Buildings()
+            {
+                BuildingName = "supply_node",
+                MaxLevel = 1,
+                IsProvincesBuilding = false
+            });
+            state.Buildings.Add(new Buildings()
+            {
+                BuildingName = "naval_base",
+                MaxLevel = 10,
+                IsProvincesBuilding = true
+            });
+            state.Buildings.Add(new Buildings()
+            {
+                BuildingName = "bunker",
+                MaxLevel = 10,
+                IsProvincesBuilding = true
+            });
+            state.Buildings.Add(new Buildings()
+            {
+                BuildingName = "coastal_bunker",
+                MaxLevel = 10,
+                IsProvincesBuilding = true
+            });
+            state.Buildings.Add(new Buildings()
+            {
+                BuildingName = "dockyard",
+                MaxLevel = 20,
+                IsProvincesBuilding = false
+            });
+            state.Buildings.Add(new Buildings()
+            {
+                BuildingName = "anti_air_building",
+                MaxLevel = 5,
+                IsProvincesBuilding = false
+            });
+            //state.Buildings.Add(new Buildings()
+            //{
+            //    BuildingName = "fuel_silo",
+            //    MaxLevel = 5,
+            //    IsProvincesBuilding = false
+            //});
+            state.Buildings.Add(new Buildings()
+            {
+                BuildingName = "radar_station",
+                MaxLevel = 6,
+                IsProvincesBuilding = false
+            });
+            state.Buildings.Add(new Buildings()
+            {
+                BuildingName = "rocket_site",
+                MaxLevel = 5,
+                IsProvincesBuilding = false
+            });
+            state.Buildings.Add(new Buildings()
+            {
+                BuildingName = "nuclear_reactor",
+                MaxLevel = 1,
+                IsProvincesBuilding = false
+            });
+
             using var stream = new FileStream("stateSettings.json", FileMode.Create);
             using var write = new StreamWriter(stream);
             write.Write(JsonConvert.SerializeObject(state, Formatting.Indented));
@@ -36,12 +175,23 @@ namespace Test
 
         public class StateSettings
         {
-            /// <summary>
-            /// Key为地块类别, Value为地块提供的建筑槽位
-            /// </summary>
-            public readonly Dictionary<string, byte>? StateCategory = new(16);
+            public readonly List<StateCategory> StateCategory = new(16);
+            public readonly List<Buildings> Buildings = new(20);
             public int MinManpower { get; set; }
             public int MaxManpower { get; set; }
+        }
+
+        public class StateCategory
+        {
+            public string? Type { get; set; }
+            public byte? Slot { get; set; }
+        }
+
+        public class Buildings
+        {
+            public string? BuildingName { get; set; }
+            public byte MaxLevel { get; set; }
+            public bool IsProvincesBuilding { get; set; }
         }
     }
 }
